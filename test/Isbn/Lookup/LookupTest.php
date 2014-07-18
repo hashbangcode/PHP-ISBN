698f073758d359cd3559b1ef6b31cab62c2a4848
Philip Norton <philipnorton42@gmail.com>
2014-07-18 15:24:21 +0100

    adding a google test case for the lookup service

--- test/Isbn/Lookup/LookupTest.php
+++ test/Isbn/Lookup/LookupTest.php
@@ -19,7 +19,7 @@ class LookupTest extends PHPUnit_Framework_TestCase {
     $this->amazonAssociateTag = $GLOBALS['amazonAssociateTag'];
   }  
   
-  public function testCreation() {
+  public function testAmazonCreation() {
 
     $service = new Lookup\Service\Amazon($this->amazonAccessKeyID, $this->amazonSecretKey, $this->amazonAssociateTag);
     $this->assertInstanceOf('Isbn\Isbn\Lookup\Service\ServiceInterface', $service);
@@ -29,11 +29,66 @@ class LookupTest extends PHPUnit_Framework_TestCase {
     $this->assertInstanceOf('Isbn\Isbn\Lookup\Lookup', $lookup);
   }
   
-  public function testBookLookup() {
+  public function testAmazonBookLookup($isbn) {    
     $service = new Lookup\Service\Amazon($this->amazonAccessKeyID, $this->amazonSecretKey, $this->amazonAssociateTag);
     $lookup = new Lookup\Lookup($service);
 
-    $book = $lookup->getBook('9781405251426');
+    $book = $lookup->getBook($isbn);
     $this->assertInstanceOf('Isbn\Book', $book);
   }
+  
+  /**
+   * @dataProvider isbnNumbersProvider
+   */  
+  public function testGoogleBookLookup($isbn) {    
+    $service = new Lookup\Service\Google();
+    $lookup = new Lookup\Lookup($service);
+
+    $book = $lookup->getBook($isbn);
+    $this->assertInstanceOf('Isbn\Book', $book);
+  }  
+  
+  /**
+   * @dataProvider isbnNumbersProvider
+   */
+  public function isbnNumbersProvider() {
+    return array(
+      array('9780552167758'),
+      array('9781743007419'),
+      array('9780752488110'),
+      array('9781848317260'),
+      array('9781781855898'),
+      array('9780007424832'),
+      array('9781444000177'),
+      array('9780723292098'),
+      array('9780723288589'),
+      array('9780857534057'),
+      array('9780099580867'),
+      array('9780670919635'),
+      array('9781607069461'),
+      array('9780440870692'),
+      array('9781405235242'),
+      array('9781405235174'),
+      array('9781405235709'),
+      array('9781405235266'),
+      array('9781405235648'),
+      array('9781405235259'),
+      array('9780141340067'),
+      array('9781405235617'),
+      array('9781471129483'),
+      array('9781405918237'),
+      array('9780091947309'),
+      array('9780857831026'),
+      array('9781408836453'),
+      array('9780749958763'),
+      array('9781408842454'),
+      array('9780224087889'),
+      array('9780857053091'),
+      array('9781447256779'),
+      array('9780718177058'),
+      array('9781444786323'),
+      array('9780099585152'),
+      array('9780755378494')
+    );
+  }  
 }
