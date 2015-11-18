0dfe03ac50451736338453c797b90a276ab24a1f
Philip Norton <philipnorton42@gmail.com>
2015-11-18 10:51:17 +0000

    added a couple of single tests to the Nielsen and Google test classes to test for single books

--- test/Isbn/Lookup/Service/NielsenTest.php
+++ test/Isbn/Lookup/Service/NielsenTest.php
@@ -25,14 +25,38 @@ class NielsenTest extends ServiceTestCase {
   public function testGetaDataResults($isbn) {
 
     $nielsen = new Nielsen($this->clientId, $this->password);
-    
+
     $books = $nielsen->getMetadataFromIsbn($isbn);
-   
+
+    if ($books == FALSE) {
+        print_r($nielsen->getErrors());
+    }
+
     $this->assertTrue(is_array($books));
     $this->assertInstanceOf('Isbn\Book', $books[0]);
     $this->assertEquals($isbn, $books[0]->getIsbn());
-    
+
     // Optionally save the image data out (to the cache directory)
-    // $books[0]->convertBase64Image('./cache/images');
+    $books[0]->convertBase64Image('./cache/images');
   }
+
+    public function testGetaSingleDataResults() {
+
+      $isbn = '9781408863114';
+
+      $nielsen = new Nielsen($this->clientId, $this->password);
+
+      $books = $nielsen->getMetadataFromIsbn($isbn);
+
+      if ($books == FALSE) {
+          print_r($nielsen->getErrors());
+      }
+
+      $this->assertTrue(is_array($books));
+      $this->assertInstanceOf('Isbn\Book', $books[0]);
+      $this->assertEquals($isbn, $books[0]->getIsbn());
+
+      // Optionally save the image data out (to the cache directory)
+      $books[0]->convertBase64Image('./cache/images');
+    }
 }
