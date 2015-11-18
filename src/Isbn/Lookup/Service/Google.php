b041072c8b447f4f48d70ce393c8da36dccbe2a7
Philip Norton <philipnorton42@gmail.com>
2015-11-18 10:49:00 +0000

    format tweaking to the google service class

--- src/Isbn/Lookup/Service/Google.php
+++ src/Isbn/Lookup/Service/Google.php
@@ -12,15 +12,15 @@ namespace Isbn\Isbn\Lookup\Service;
 class Google extends Service {
 
   protected $apiKey = false;
-  
+
   public function __construct() {
-    
+
     parent::__construct();
   }
 
   /**
    * Sets the API key for use in the Google API lookup. When the API key is used
-   * the data returned will include user-specific information, such as 
+   * the data returned will include user-specific information, such as
    * purchased status.
    *
    * @param string $apiKey The API key to be used.
@@ -28,14 +28,14 @@ class Google extends Service {
   public function setApiKey($apiKey) {
     $this->apiKey = $apiKey;
   }
-  
+
   /**
    *
    * @param type $isbn
    * @return boolean|\Isbn\Book
    */
   public function getMetadataFromIsbn($isbn) {
-       
+
     // Reset errors.
     $this->errors = array();
 
@@ -47,32 +47,32 @@ class Google extends Service {
     $books = array();
 
     $url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn;
-        
+
     if ($this->apiKey !== false) {
       $url .= '&key=' . $this->apiKey;
     }
 
     $this->rawData = file_get_contents($url);
-    
+
     if ($this->rawData === false) {
       $this->setError('-1', 'Communication error.');
       return false;
     }
 
-    $data = json_decode($this->rawData);   
-    
+    $data = json_decode($this->rawData);
+
     if (!isset($data->items)) {
       return false;
     }
 
     foreach ($data->items as $item) {
-      
+
       if (isset($item->volumeInfo->subtitle)) {
           $title = $item->volumeInfo->title . ': ' . $item->volumeInfo->subtitle;
       } else {
         $title = $item->volumeInfo->title;
       }
-    
+
       $book = new \Isbn\Book($title, $isbn);
 
       if (isset($item->volumeInfo->publisher)) {
@@ -82,13 +82,13 @@ class Google extends Service {
       if (isset($item->volumeInfo->publishedDate)) {
         $book->setPublicationDate($item->volumeInfo->publishedDate);
       }
-      
+
       if (isset($item->volumeInfo->authors)) {
         foreach ($item->volumeInfo->authors as $author) {
           $book->setAuthor($author);
         }
       }
-      
+
       if (isset($item->volumeInfo->pageCount)) {
         $book->setPages($item->volumeInfo->pageCount);
       }
@@ -100,8 +100,8 @@ class Google extends Service {
       $books[] = $book;
     }
 
-    $this->cache->set('google' . $isbn, serialize($books));    
-    
+    $this->cache->set('google' . $isbn, serialize($books));
+
     return $books;
   }
 
